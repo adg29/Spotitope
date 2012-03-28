52c3646520452577388edb99e9cfc2311a809d76
Alan Garcia <alang@Alans-MacBook-Pro.local>
2012-03-28 18:17:53 -0400

    This app has a high voltage static view with backbone and bootstrap front end for searching the spotify metadata api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotitope::Application.config.secret_token = '1777fc980c1fed703f2f5c65a496ca741bb0eae565e390b80dc0b6a6347b5070be3fcfa3a1e31e509f9b07dfe312322138c949757bf5de1c75683ecad85904ee'
